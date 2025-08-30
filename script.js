a733e421faabb982e0ad4e4709cc28eea5855ffb
Vesko <vesselin60@gmail.com>
2025-08-30 09:00:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// console.log(1)
+
+// alert("Hello");
+
+// const result = confirm('Goocho is here.')
+// console.log(result)
+
+// const answer = +prompt("Are you of age?", "")
+// console.log(answer + 5)
+// console.log(typeof answer)
+
+// const answers = [];
+
+// answers[0] = prompt("What is your name?", "");
+// answers[1] = prompt("What is your surname?", "");
+// answers[2] = prompt("How old are you?", "");
+
+// document.writeln(answers);
+
+// console.log(typeof answers)
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}`)
+
+const user = 'Gocho';
+
+console.log(`Hello, ${user}`);
